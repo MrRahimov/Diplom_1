38dbc633a8b939964f33df9ad5a4a6d5b10dbbc3
Elmar <96076035+MrRahimov@users.noreply.github.com>
2025-11-09 22:55:09 +0200

    Rename and refactor burger test functions

--- tests/test_burger.py
+++ tests/test_burger.py
@@ -2,14 +2,50 @@ import pytest
 from praktikum.burger import Burger
 
 
-def test_burger_starts_empty():
+def test_init_creates_empty_ingredients():
     burger = Burger()
     assert burger.ingredients == []
 
 
+def test_set_buns_sets_bun(bun):
+    burger = Burger()
+    burger.set_buns(bun)
+    assert burger.bun == bun
+
+
+def test_add_ingredient_appends_to_ingredients(bun, meat):
+    burger = Burger()
+    burger.set_buns(bun)
+    burger.add_ingredient(meat)
+    assert burger.ingredients == [meat]
+
+
+def test_remove_ingredient_removes_by_index(bun, meat, sauce):
+    burger = Burger()
+    burger.set_buns(bun)
+    burger.add_ingredient(meat)
+    burger.add_ingredient(sauce)
+
+    burger.remove_ingredient(0)
+
+    assert burger.ingredients == [sauce]
+
+
+def test_move_ingredient_changes_order(bun, meat, sauce):
+    burger = Burger()
+    burger.set_buns(bun)
+    burger.add_ingredient(meat)   # index 0
+    burger.add_ingredient(sauce)  # index 1
+
+    burger.move_ingredient(1, 0)
+
+    assert burger.ingredients == [sauce, meat]
+
+
 @pytest.mark.parametrize("bun_price", [50.0, 100.0, 199.99])
-def test_set_buns_affects_price(bun, meat, bun_price):
+def test_get_price_uses_bun_and_ingredients(bun, meat, bun_price):
     bun.get_price.return_value = bun_price
+
     burger = Burger()
     burger.set_buns(bun)
     burger.add_ingredient(meat)
@@ -18,42 +54,32 @@ def test_set_buns_affects_price(bun, meat, bun_price):
     assert burger.get_price() == pytest.approx(expected)
 
 
-def test_add_and_remove_ingredient(bun, meat, sauce):
-    burger = Burger()
-    burger.set_buns(bun)
-    burger.add_ingredient(meat)
-    burger.add_ingredient(sauce)
-
-    assert len(burger.ingredients) == 2
-
-    burger.remove_ingredient(0)
-    assert [i.get_name() for i in burger.ingredients] == [
-        sauce.get_name.return_value
-    ]
-
-
-def test_move_ingredient(bun, meat, sauce):
-    burger = Burger()
-    burger.set_buns(bun)
-    burger.add_ingredient(meat)   
-    burger.add_ingredient(sauce)  
-
-    burger.move_ingredient(1, 0)
-
-    assert [i.get_name() for i in burger.ingredients] == [
-        sauce.get_name.return_value,
-        meat.get_name.return_value,
-    ]
-
-
-def test_receipt_contains_names_and_total(bun, meat):
+def test_get_receipt_contains_bun_names(bun, meat):
     burger = Burger()
     burger.set_buns(bun)
     burger.add_ingredient(meat)
 
     receipt = burger.get_receipt()
-    total = 2 * bun.get_price.return_value + meat.get_price.return_value
 
     assert bun.get_name.return_value in receipt
+
+
+def test_get_receipt_contains_ingredient_name(bun, meat):
+    burger = Burger()
+    burger.set_buns(bun)
+    burger.add_ingredient(meat)
+
+    receipt = burger.get_receipt()
+
     assert meat.get_name.return_value in receipt
+
+
+def test_get_receipt_contains_total_price(bun, meat):
+    burger = Burger()
+    burger.set_buns(bun)
+    burger.add_ingredient(meat)
+
+    total = 2 * bun.get_price.return_value + meat.get_price.return_value
+    receipt = burger.get_receipt()
+
     assert str(total) in receipt
